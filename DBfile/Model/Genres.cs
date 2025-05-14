6ef55d00ecaaaf32981393c3c6ee982ff7fe2951
ac146842 <ac146842@8CG8163L11>
2025-05-15 11:17:40 +1200

    Add project files.

---
+++ DBfile/Model/Genres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDBApp.DBfile.Model
+{
+
+    public class Genres
+    {
+        private string Genre_Name { get; set; }
+        private int Genre_ID { get; set; }
+
+        public Genres(string inGenre, int inGenre_ID)
+        {
+            Genre_Name = inGenre;
+            Genre_ID = inGenre_ID;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicDBApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
