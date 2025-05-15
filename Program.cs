ed03f0640783a0fbb5af242fb7f248dde57aff27
ac146842 <ac146842@8CG8163L11>
2025-05-16 09:57:55 +1200

    First attempt at connecting to the database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace MusicDBApp
+﻿using MusicDBApp.Repositories;
+
+namespace MusicDBApp
 {
-    internal class Program
+    public class Program
     {
+        private static StorageManager storageManager;       
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=MusicDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            storageManager = new StorageManager(connectionString);
         }
     }
 }
