75d5cd0062560fb3e24b8cb36f7282e42bf61e81
ac146842 <ac146842@8CG8163L11>
2025-05-16 09:48:26 +1200

    Added StorageManager connection to DB and put in try catch

--- Repositories/StorageManager.cs
+++ Repositories/StorageManager.cs
@@ -11,5 +11,26 @@ namespace MusicDBApp.Repositories
     {
         private SqlConnection conn;
 
+        public StorageManager(string connectionString)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                Console.WriteLine("Connection successful");
+            }
+
+            catch (SqlException e)
+            {
+                Console.WriteLine("Connection is NOT successful \n");
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occoured \n");
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
